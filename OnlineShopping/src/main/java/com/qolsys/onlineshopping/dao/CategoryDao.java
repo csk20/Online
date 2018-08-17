ab412fb388e0bbafcbf137c2aea4118ff243c6fe
qolsys <qolsys@qolsys-desktop>
2018-08-17 19:32:34 +0530

    Second commit

---
+++ OnlineShopping/src/main/java/com/qolsys/onlineshopping/dao/CategoryDao.java
@@ -0,0 +1,11 @@
+package com.qolsys.onlineshopping.dao;
+
+import java.util.List;
+
+import com.qolsys.onlineshopping.dto.Category;
+
+public interface CategoryDao {
+	
+	List<Category> list();
+
+}
---
+++ OnlineShopping/src/main/java/com/qolsys/onlineshopping/daoImpl/CategoryDaoImpl.java
@@ -0,0 +1,22 @@
+package com.qolsys.onlineshopping.daoImpl;
+
+import java.util.List;
+
+import com.qolsys.onlineshopping.dao.CategoryDao;
+import com.qolsys.onlineshopping.dto.Category;
+
+public class CategoryDaoImpl implements CategoryDao {
+
+	@Override
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+
+	
+	
+
+}
